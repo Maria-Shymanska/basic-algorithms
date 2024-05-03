f00cba9b0e2a0b63fdc83ad8161bbd3b37a38f8e
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2024-05-03 19:21:47 +0200

    Update algorithms_04.py

--- algorithms_04.py
+++ algorithms_04.py
@@ -3,10 +3,101 @@
 def bubble_sort(lst):
     n = len(lst)
     for i in range(n-1):
-        for j in range(0, n-i-1): 
+        for j in range(0, n-i-1):
+            print(i, j, lst)
             if lst[j] > lst[j+1] :
-                lst[j], lst[j+1] = lst[j+1], lst[j] 
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+            print('--')
+        print('----')
     return lst
 
 numbers = [5, 3, 8, 4, 2]
 bubble_sort(numbers)
+
+# Insertion sort
+
+def insertion_sort(lst):
+    for i in range(1, len(lst)):
+        key = lst[i]
+        j = i-1
+        print(i, j, lst)
+        while j >=0 and key < lst[j] :
+                lst[j+1] = lst[j]
+                j -= 1
+        lst[j+1] = key 
+    return lst
+print()
+
+numbers = [5, 3, 8, 4, 2]
+insertion_sort(numbers)
+
+
+# Selection Sort
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+numbers = [5, 3, 8, 4, 2]
+print(selection_sort(numbers))
+
+# Quick sort
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+print(quicksort([5, 3, 8, 4, 2]))
+# Виведе: [2, 3, 4, 5, 8]
+
+
+# Merge sort
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    return merge(merge_sort(left_half), merge_sort(right_half))
+
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    # Спочатку об'єднайте менші елементи
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
+    # Якщо в лівій або правій половині залишилися елементи, 
+		# додайте їх до результату
+    while left_index < len(left):
+        merged.append(left[left_index])
+        left_index += 1
+
+    while right_index < len(right):
+        merged.append(right[right_index])
+        right_index += 1
+
+    return merged
+
