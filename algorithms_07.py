4577bd5f5c1708e1a9d0bcffd0e9b154561505b1
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2024-05-15 20:20:46 +0200

    Update algorithms_07.py

--- algorithms_07.py
+++ algorithms_07.py
@@ -1,1 +1,212 @@
 
+'''Наведемо приклад базової реалізації дерева:'''
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+'''Реалізація прямого обходу дерева:'''
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def preorder_traversal(root):
+    if root:
+        print(root.val)
+        preorder_traversal(root.left)
+        preorder_traversal(root.right)
+
+# Створення дерева
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Прямий обхід:")
+preorder_traversal(root)
+
+
+
+'''Реалізація центрового обходу дерева:'''
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.val)
+        inorder_traversal(root.right)
+
+# Створення дерева
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Центровий обхід:")
+inorder_traversal(root)
+
+
+
+'''Реалізація зворотного обходу дерева:'''
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def postorder_traversal(root):
+    if root:
+        postorder_traversal(root.left)
+        postorder_traversal(root.right)
+        print(root.val)
+
+# Створення дерева
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Зворотний обхід:")
+postorder_traversal(root)
+
+
+
+'''Двійкове дерево пошуку (ДДП, бінарні дерева пошуку) — це деревоподібна структура даних, що має такі властивості:
+
+
+
+1. Кожен вузол має ключ.
+
+2. Ключі в лівому піддереві будь-якого вузла завжди менші за його ключ.
+
+3. Ключі в правому піддереві будь-якого вузла завжди більші за його ключ.
+
+4. Ліве та праве піддерева також є ДДП.'''
+
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+# Test
+root = Node(5)
+root = insert(root, 3)
+root = insert(root, 2)
+root = insert(root, 4)
+root = insert(root, 7)
+root = insert(root, 6)
+root = insert(root, 8)
+
+# Пошук значення
+val =  4
+result = search(root, val)
+if result:
+    print(f"У дереві знайдено значення {result.val}")
+else:
+    print(f"У дереві не знайдено значення {val}")
+
+
+
+# Тепер розглянемо операцію видалення та відповідну функцію delete. 
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\\t" * level + prefix + str(self.val) + "\\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+def min_value_node(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current
+
+def delete(root, key):
+    if not root:
+        return root
+
+    if key < root.val:
+        root.left = delete(root.left, key)
+    elif key > root.val:
+        root.right = delete(root.right, key)
+    else:
+        if not root.left:
+            temp = root.right
+            root = None
+            return temp
+        elif not root.right:
+            temp = root.left
+            root = None
+            return temp
+        root.val = min_value_node(root.right).val
+        root.right = delete(root.right, root.val)
+    return root
+
+# Test
+root = Node(5)
+root = insert(root, 3)
+root = insert(root, 2)
+root = insert(root, 4)
+root = insert(root, 7)
+root = insert(root, 6)
+root = insert(root, 8)
+
+root = delete(root, 7)
+print(root)
+
